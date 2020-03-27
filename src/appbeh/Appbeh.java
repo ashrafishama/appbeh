02bbbc232f6ad25a572f5dc3414fcb231fe0cae3
Khandakar Ashrafi Akbar <aninditaashrafi@gmail.com>
2020-03-27 00:32:06 -0400

    files

---
+++ src/appbeh/Appbeh.java
@@ -0,0 +1,12 @@
+package appbeh;
+/*
+  @author ashrafi
+ */
+public class Appbeh extends TTPs {
+    public static void main(String[] args) {
+       /*TTPs ttps = new TTPs();
+       String s = "Hello";
+       ttps.T1156(s);*/
+    }
+    
+}
---
+++ src/appbeh/TTPs.java
@@ -0,0 +1,10 @@
+package appbeh;
+/*
+ * @author ashrafi
+ */
+public class TTPs {
+    //public String line;
+    public static void T1156(String S){
+        System.out.println("Length of the string is " + S.length());
+    }
+}
