8be07cdb716384d7d45a12c9e6731bdba3f40cb6
Khandakar Ashrafi Akbar <aninditaashrafi@gmail.com>
2020-03-27 11:12:08 -0400

    change added in index for original program data

--- src/appbeh/TTPs.java
+++ src/appbeh/TTPs.java
@@ -8,28 +8,28 @@ public class TTPs {
     //public String line;
     public static int T1156(String S[],int length){ //T1156-1 & T1156-2
         //System.err.println(Arrays.toString(S));
-        if(length==5){ //line containing "File:" found
-            String[] filePathParts = S[3].split("/");
-            if(filePathParts.length>=2){
-                if(S[2].equals("open") && (filePathParts[1].equals("home") || filePathParts[1].equals("root")) && 
+        if(length==7){ //line containing "File:" found
+            String[] filePathParts = S[4].split("/");
+            if(filePathParts.length>=2){ //to address corner case for filepath e.g. NA
+                if(S[3].equals("open") && (filePathParts[1].equals("home") || filePathParts[1].equals("root")) && 
                         (filePathParts[filePathParts.length-1].equals(".bash_profile") || filePathParts[filePathParts.length-1].equals(".bashrc"))){
                     return 1; //subpart 1
                }
             }
         }
-        if(length==5){ //line containing "File:" found
-            String[] filePathParts = S[3].split("/");
-            if(filePathParts.length>=2){
-                if(S[2].equals("dup") && (filePathParts[1].equals("home") || filePathParts[1].equals("root")) && 
+        if(length==7){ //line containing "File:" found
+            String[] filePathParts = S[4].split("/");
+            if(filePathParts.length>=2){ //to address corner case for filepath e.g. NA
+                if(S[3].equals("dup") && (filePathParts[1].equals("home") || filePathParts[1].equals("root")) && 
                     (filePathParts[filePathParts.length-1].equals(".bash_profile") || filePathParts[filePathParts.length-1].equals(".bashrc"))){
                     return 2; //subpart 2
                 }
             }
         }
-        if(length==5){ //line containing "File:" found
-            String[] filePathParts = S[3].split("/");
-            if(filePathParts.length>=2){
-                if(S[2].equals("write") && (filePathParts[1].equals("home") || filePathParts[1].equals("root")) && 
+        if(length==7){ //line containing "File:" found
+            String[] filePathParts = S[4].split("/");
+            if(filePathParts.length>=2){ //to address corner case for filepath e.g. NA
+                if(S[3].equals("write") && (filePathParts[1].equals("home") || filePathParts[1].equals("root")) && 
                     (filePathParts[filePathParts.length-1].equals(".bash_profile") || filePathParts[filePathParts.length-1].equals(".bashrc"))){
                     return 3; //subpart 3
                 }
