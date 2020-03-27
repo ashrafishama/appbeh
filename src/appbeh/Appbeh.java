bf106b9ed4001e72ceed314d782a5a77b874e1a2
Khandakar Ashrafi Akbar <aninditaashrafi@gmail.com>
2020-03-27 10:09:07 -0400

    skeleton completed

--- src/appbeh/Appbeh.java
+++ src/appbeh/Appbeh.java
@@ -1,24 +1,27 @@
 package appbeh;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.DatagramSocketImplFactory;
-import java.util.ArrayList;
+import java.util.*; 
+import java.io.*;
 /*
   @author ashrafi
  */
 public class Appbeh extends TTPs {
     public static void main(String[] args) throws IOException {
-       int[] TTPcounter = new int[39]; //each index for distinct TTP appearance count
-       /*TTPs ttps = new TTPs();
-       String s = "Hello";
-       ttps.T1156(s);*/
-       String filename = "chrome.txt";
+       int[] TTPfrequency = new int[39]; //each index for distinct TTP appearance count
+       Arrays.fill(TTPfrequency, 0); //initial frequency '0'
+       TTPs ttps = new TTPs();
+       ArrayList<ArrayList<Integer>> TTPmatcher = new ArrayList<ArrayList<Integer>>(); //39 entries for 39 TTPs for matching parts
+       TTPmatcher.add(new ArrayList<Integer>(Arrays.asList(1, 2, 3))); //3 subpart for TTP T1156
+       
+       //String filename = "chrome.txt";
+       String filename = "test.txt";
        ArrayList content = fileRead(filename);
-       System.err.println("Size of file is " + content.size());
-       parser(content);
+       parser(content,ttps,TTPfrequency);
+       
+       
+       
     }
+    
+    //file reader
     public static ArrayList fileRead(String filename) throws IOException{
         BufferedReader bufReader = new BufferedReader(new FileReader(filename));
         ArrayList<String> listOfLines = new ArrayList<>();
@@ -31,14 +34,29 @@ public class Appbeh extends TTPs {
         return listOfLines;
     }
     
-    public static void parser(ArrayList data){
+    //line parser (with delemeter)
+    public static void parser(ArrayList data,TTPs ttps,int[] ttpFrequency){
         for(int i=0;i<data.size();i++){
             String temp = (String) data.get(i);
             String[] parts = temp.split("\\s+");
-            /*if(parts[0].equals("File:")){
-                System.err.println("found");
+            int match = ttps.T1156(parts,parts.length); //have to call this function for each TTP, against each line, returns a #
+            //System.err.println("Matched " + match);
+            /*matchUpdater(data, 1, match);
+            if(allPartsMatched(data,1)){
+                ttpFrequency[0]+=1;
             }*/
         }
         return;
+    }
+    
+    public static void matchUpdater(ArrayList<ArrayList<Integer>> ttpMatcher,int TTPNo,int part){
+        ttpMatcher.get(TTPNo-1).set(part-1,0); //make the matched part 0
+    }
+    
+    public static boolean allPartsMatched(ArrayList<ArrayList<Integer>> ttpMatcher,int TTPNo){
+        if (Collections.frequency(ttpMatcher.get(TTPNo-1), 0) == ttpMatcher.get(TTPNo-1).size()){ //TTPNo-1 is the index in the AL
+            return true;
+        }        
+        return false;
     }
 }
--- src/appbeh/TTPs.java
+++ src/appbeh/TTPs.java
@@ -1,20 +1,35 @@
 package appbeh;
 
-import sun.security.util.Length;
-
+import java.util.Arrays;
 /*
  * @author ashrafi
  */
 public class TTPs {
     //public String line;
-    public static boolean T1156(String S[],int length){ //T1156-1 & T1156-2
+    public static int T1156(String S[],int length){ //T1156-1 & T1156-2
+        //System.err.println(Arrays.toString(S));
+        
         if(length==5){ //line containing "File:" found
             String[] filePathParts = S[3].split("/");
-            if(filePathParts[2].equals("write") && (filePathParts[0].equals("home") || filePathParts[0].equals("root")) && 
+            if(filePathParts[0].equals("open") && (filePathParts[1].equals("home") || filePathParts[1].equals("root")) && 
                     (filePathParts[length-1].equals(".bash_profile") || filePathParts[length-1].equals(".bashrc"))){
-                return true;
+                return 1; //subpart 1
             }
         }
-        return false; //if no hard match found to this TTP
+        if(length==5){ //line containing "File:" found
+            String[] filePathParts = S[3].split("/");
+            if(filePathParts[0].equals("dup") && (filePathParts[1].equals("home") || filePathParts[1].equals("root")) && 
+                    (filePathParts[length-1].equals(".bash_profile") || filePathParts[length-1].equals(".bashrc"))){
+                return 2; //subpart 2
+            }
+        }
+        if(length==5){ //line containing "File:" found
+            String[] filePathParts = S[3].split("/");
+            if(filePathParts[0].equals("write") && (filePathParts[1].equals("home") || filePathParts[1].equals("root")) && 
+                    (filePathParts[length-1].equals(".bash_profile") || filePathParts[length-1].equals(".bashrc"))){
+                return 3; //subpart 3
+            }
+        }
+        return 0; //if no hard match found to this TTP
     }
 }
