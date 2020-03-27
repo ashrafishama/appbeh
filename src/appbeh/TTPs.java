483c57af31a4bd5aff58124108ac6b80b1a6994e
Khandakar Ashrafi Akbar <aninditaashrafi@gmail.com>
2020-03-27 02:18:30 -0400

    new change

--- src/appbeh/TTPs.java
+++ src/appbeh/TTPs.java
@@ -1,14 +1,20 @@
 package appbeh;
+
+import sun.security.util.Length;
+
 /*
  * @author ashrafi
  */
 public class TTPs {
     //public String line;
-    public static void T1156(String S){ //T1156-1 & T1156-2
-        int length = S.length();
+    public static boolean T1156(String S[],int length){ //T1156-1 & T1156-2
         if(length==5){ //line containing "File:" found
-            
+            String[] filePathParts = S[3].split("/");
+            if(filePathParts[2].equals("write") && (filePathParts[0].equals("home") || filePathParts[0].equals("root")) && 
+                    (filePathParts[length-1].equals(".bash_profile") || filePathParts[length-1].equals(".bashrc"))){
+                return true;
+            }
         }
+        return false; //if no hard match found to this TTP
     }
-
 }
